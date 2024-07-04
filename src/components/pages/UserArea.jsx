60ef77c171a9ae49810160f8b7f88fa8e51da46c
MelkysedekDev <melkybahia88@gmail.com>
2024-07-03 22:01:34 -0300

    Modify UserArea

--- src/components/pages/UserArea.jsx
+++ src/components/pages/UserArea.jsx
@@ -1,3 +1,10 @@
+import LinkButton from "../layout/LinkButton";
+
 export default function UserArea(){
-    return <h1>Área de usuário</h1>
-}
+    return (
+        <>
+            <h1>Área de usuário</h1>
+            <LinkButton to='/ProjetoLovelace/createactivity' text='Criar atividade'/>
+        </>
+        )
+    }
